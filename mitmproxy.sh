ab034958546abe896db7c42120cc7f48f259d88b
Jon Ballands <jballands@gmail.com>
2025-02-11 16:05:13 -0600

    Updating mitmproxy script

--- mitmproxy.sh
+++ mitmproxy.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+
+# Check if mitmproxy is installed
+if ! command -v mitmproxy &> /dev/null; then
+    echo "mitmproxy is not installed. Install it with brew then rerun this script."
+    exit 1
+fi
+
 # the tail command will skip the first line because it's an informational message
 interfaces="$(networksetup -listallnetworkservices | tail -n +2)"
+
+# Function to clean up proxy settings
+cleanup() {
+    echo -e "\nCleaning up proxy settings..."
+    for interface in $interfaces; do
+        echo "Disabling proxy on $interface"
+        networksetup -setwebproxystate "$interface" off
+        networksetup -setsecurewebproxystate "$interface" off
+    done
+    exit 0
+}
+
+# Set up trap for Ctrl+C
+trap cleanup SIGINT
 
 IFS=$'\n' # split on newlines in the for loops
 
@@ -15,8 +36,5 @@ done
 
 mitmproxy
 
-for interface in $interfaces; do
-  echo "Disabling proxy on $interface"
-  networksetup -setwebproxystate "$interface" off
-  networksetup -setsecurewebproxystate "$interface" off
-done
+# Run cleanup even after normal exit
+cleanup
